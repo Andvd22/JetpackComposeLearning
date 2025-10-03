6584b43035431ca588a1fc2922301486b75c8a6d
andv <dovanan774@gmail.com>
2025-10-03 20:13:07 +0700

    First commit - Android project

---
+++ app/src/main/java/com/example/jetpackcomposelearning/JetpackCompose.kt
@@ -0,0 +1,12 @@
+package com.example.jetpackcomposelearning
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun Greet(){
+    Text("hello")
+
+
+}
