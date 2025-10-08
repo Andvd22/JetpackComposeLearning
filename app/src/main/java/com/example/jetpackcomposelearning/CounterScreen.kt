e502919d3e8d4e5ccd1ebdda8072a0598bddaa03
Andvd22 <dovanan774@gmail.com>
2025-10-08 09:18:15 +0700

    using_modifier

--- app/src/main/java/com/example/jetpackcomposelearning/CounterScreen.kt
+++ app/src/main/java/com/example/jetpackcomposelearning/CounterScreen.kt
@@ -1,5 +1,9 @@
 package com.example.jetpackcomposelearning
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,16 +12,26 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CounterScreen() {
     var count by remember { mutableStateOf(0) }
 
-    Text("$count")
+    Column {
+        Text("$count",
+            modifier = Modifier.padding(16.dp),
+            color = colorResource(R.color.teal_200)
+        )
 
-    Button(onClick = {
-        count++;
-    }) {
-        Text("increase")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            count++;
+        }) {
+            Text("increase")
+        }
     }
+
 }
